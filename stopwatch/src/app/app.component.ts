8b47a1243687f449e8489a880b6ddc35f305c015
Aleksandr <alex.avramenko2004@gmail.com>
2023-01-18 20:09:57 -0500

    Creating new app called StopWatch

---
+++ stopwatch/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'stopwatch';
+
+
+  ngOnInit(): void {
+    const seconds = interval(1000);
+  }
+
+}
